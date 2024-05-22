1b95f4ce69a521fefaa8409fe272cf13862d692f
haider ali <haiderali333222@gmail.com>
2024-05-23 02:08:48 +0500

    first commit

---
+++ views/files.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, File, UploadFile
+from typing import List
+from controllers.files import FileUpload
+
+files_router = APIRouter()
+
+
+@files_router.get("/")
+async def root():
+    return {"message": "File Upload Example. Please use `/docs` for enter to Swagger UI and test the API"}
+
+
+@files_router.post("/upload_files/")
+async def upload_file_queue(files_list: List[UploadFile] = File(...)):
+    try:
+        print("here")
+        files_number = FileUpload.upload_files(files_list)
+        print(files_number)
+        return {"Number of files will be processed": files_number}
+    except Exception as e:
+        print(e)
