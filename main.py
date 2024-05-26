e899853440b4ae9f43af912848c88a74be51c588
haider ali <haiderali333222@gmail.com>
2024-05-26 21:15:29 +0500

    add fixes

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from api.routes.files import files_router
 from fastapi import FastAPI, APIRouter, Request
 from fastapi.responses import JSONResponse
 from utils.slack import slack_notify_error
+import subprocess
+
 
 app = FastAPI()
 api_router = APIRouter()
@@ -17,4 +19,15 @@ async def global_exception_handler(request: Request, exc: Exception):
     return JSONResponse(status_code=500, content={"message": "Internal Server Error"})
 
 if __name__ == '__main__':
+    
+    celery_beat_cmd = ["celery", "--app", "services.celery.celery_app", "beat"]
+    celery_beat_process = subprocess.Popen(celery_beat_cmd)
+
+        # Start the Celery worker in a separate process
+    celery_worker_cmd= ["celery", "--app", "services.celery.celery_app", 
+                            "worker","--concurrency=1",
+                            "-Q" ,"celery_queue_for_file_upload","--pool=solo"]
+    celery_worker_process = subprocess.Popen(celery_worker_cmd)
     uvicorn.run(app, host="0.0.0.0", port=8888)
+    celery_beat_process.terminate()
+    celery_worker_process.terminate()
--- workers/file_upload.py
+++ workers/file_upload.py
@@ -2,9 +2,13 @@ from services.celery.celery_app import celery_app
 import os
 import pandas as pd
 
-@celery_app.task(name="File Processing Task")
+@celery_app.task(name="file_handler_task")
 def file_handler_task(file: str):
-    df = pd.read_excel(file)
-    print(f"{df.head()}")
-    print(f"file processed {file}")
-    os.remove(file)
+    try:
+        print("here again")
+        df = pd.read_excel(file)
+        print(f"{df.head()}")
+        print(f"file processed {file}")
+        os.remove(file)
+    except Exception as e:
+        print(e)
