1abc6cd7077cf18f43b65fc1a2a4ebc63db2c347
haider ali <haiderali333222@gmail.com>
2024-05-27 19:15:40 +0500

    add fixes in jobs

--- jobs/competitors/onlinecomponents/fetch_store_sitemap_results.py
+++ jobs/competitors/onlinecomponents/fetch_store_sitemap_results.py
@@ -1,34 +1,13 @@
 import re
+from .helper import is_manufacturer_match, is_valid_url_and_manufacturer
 from utils.helpers.index import error_slack_message, check_manufacturer_match, url_insert_bulk,preprocess_manufacturers, get_sitemap_urls
 
 COMPETITOR = 'onlinecomponents'
 STARTING_URL = 'https://www.onlinecomponents.com/sitemaps.xml'
 
-def is_valid_url_and_manufacturer(url):
-    try:
-        pattern = r'^https://www\.onlinecomponents\.com/en/([^/]+)/([^/]+)(?:\.([^/]+))?/?$'    
-        return (True, match[1]) if (match := re.match(pattern, url)) else (False, None)
-    except Exception as e:
-        error_slack_message(e)
-        return False, None
 
 manufacturers_match_status = {}
 
-def is_manufacturer_match(manufacturers_dict, product_manufacture):
-    try: 
-        is_calculated = manufacturers_match_status.get(product_manufacture)
-        
-        if is_calculated is not None:
-            return is_calculated
-
-        is_match = check_manufacturer_match(manufacturers_dict, product_manufacture)
-
-        manufacturers_match_status[product_manufacture] = is_match
-
-        return is_match
-    except Exception as e:
-        error_slack_message(e)
-        return False
 
 def get_and_store_online_components_urls():
     try:
