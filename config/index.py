6a471741446ad585fb84632cfbc5730e84aac180
shahabbukhari <shahabbukhari098@gmail.com>
2024-05-29 11:37:39 +0500

    small changes

---
+++ config/index.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ENVIRONMENT = os.getenv("ENVIRONMENT")
+IS_DEVELOPMENT = ENVIRONMENT == "local"
+API_URL = os.getenv("AMPLIFY_API_URL")
+IS_LOCAL = ENVIRONMENT == 'local'
+IS_STAGING = ENVIRONMENT == 'staging'
+DB_URL = os.getenv('DB_URL')
+DB_NAME=os.getenv('DB_NAME')
+PROXY_CSV_URL = os.getenv('PROXY_CSV_URL')
+SLACK_HEADER = os.getenv('SLACK_HEADER')
+API_KEY_SCRAPY= os.getenv('API_KEY_SCRAPY')
+REDIS_PORT = 6380
+REDIS_DB = 0
+
+REDIS_URL = os.getenv('REDIS_URL')
