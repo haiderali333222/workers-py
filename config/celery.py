6bc59a7232265d300e108949726e7aba865d97ff
haider ali <haiderali333222@gmail.com>
2024-06-27 16:58:46 +0500

    revert celery config

--- config/celery.py
+++ config/celery.py
@@ -1,14 +1,12 @@
 from dotenv import load_dotenv
 
 from .index import REDIS_URL
-from utils.slack import send_slack_message
 
 load_dotenv()
 # Broker URL
 CELERY_BROKER_URL = REDIS_URL
 # Configure Celery
 broker_url = CELERY_BROKER_URL
-send_slack_message(CELERY_BROKER_URL)
 broker_transport_options = {"visibility_timeout": 3600}  # 1 hour.
 result_backend_transport_options = {"retry_policy": {"timeout": 5.0}}
 broker_connection_retry_on_startup = True
